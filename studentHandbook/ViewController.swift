69512fd504b6c3d591901cb560361d5b922dd02d
Rengar Tsoi <rengartsoi@Rengarde-MacBook-Pro.local>
2017-01-23 14:35:53 +0800

    Initial Commit

---
+++ studentHandbook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  studentHandbook
+//
+//  Created by Rengar Tsoi on 23/1/2017.
+//  Copyright © 2017年 Rengar Tsoi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
