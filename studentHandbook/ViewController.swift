1fa3c4a7c1f28ea531b62cfc698d68162a52312b
cowboy1222 <gennycow1222@gmail.com>
2017-03-21 14:26:52 +0800

    21/3

--- studentHandbook/ViewController.swift
+++ studentHandbook/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userEmailTextField: UITextField!
+    @IBOutlet weak var userPasswordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +21,58 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func loginButton(sender: AnyObject) {
+        
+        let userEmail = userEmailTextField.text;
+        let userPassword = userPasswordTextField.text;
+        
+        if (userEmail!.isEmpty || userPassword!.isEmpty)
+        {
+            displayMyAlertMessage(userMessage: "All fields are equired.");
+            return;
+        }
+        
+        let myUrl = URL(string: "https://lenchan139.org/myWorks/fyp/android/login.php?username=admin&password=pw")
+        let request = NSMutableURLRequest(url: myUrl!);
+        request.httpMethod = "POST";
+        
+        let postString = "userEmail=\(userEmail)&userPassword=\(userPassword)";
+        
+        request.httpBody = postString.data(using: String.Encoding.utf8);
+        
+        
+        let task = URLSession.shared.dataTask(with: myUrl!) { data, response, error in
+            guard error == nil else {
+                print(error!)
+                return
+            }
+            guard let data = data else {
+                print("Data is empty")
+                return
+            }
+            
+            let json = try! JSONSerialization.jsonObject(with: data, options: [])
+            print(json)
+        }
+        
+        task.resume()
+        
+        
+        
+    }
+    
+    func displayMyAlertMessage(userMessage:String)
+    {
+        var loginAlert = UIAlertController(title:"Alert", message:userMessage, preferredStyle:UIAlertControllerStyle.alert);
+        
+        let doneAction =  UIAlertAction(title:"done", style:UIAlertActionStyle.default, handler:nil);
+        
+        loginAlert.addAction(doneAction);
+        
+        self.present(loginAlert, animated:true, completion:nil);
+    }
+    
 
 
 }
